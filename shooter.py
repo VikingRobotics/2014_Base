17f2ae860ac3909063597e8ca8c9685a9065b3c9
Deepa <pateldr12@gmail.com>
2014-01-15 20:41:40 -0800

    Changed the shooter, added digital input, created unit tests

--- shooter.py
+++ shooter.py
@@ -9,12 +9,17 @@ class Shooter(common.ComponentBase):
     def __init__(self, config):
         self.motors = config.motors
         self.shoot_button = config.shoot_button
-
+        self.stop_input = config.stop_input
 
     def op_tick(self, time):
-        if self.shoot_button.get():
-            for motor in self.motors:
-                motor.Set(1.0)
-        else:
+        if self.stop_input.Get():
             for motor in self.motors:
                 motor.Set(0)
+        else:
+               
+            if self.shoot_button.get():
+                for motor in self.motors:
+                    motor.Set(1.0)
+            else:
+                for motor in self.motors:
+                    motor.Set(0)
