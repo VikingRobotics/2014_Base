e5a52d96559d1a32989cd2113bc45dd22cf9c0c2
viking2 <viking2@team2928.com>
2014-02-11 18:39:32 -0800

    Added initial pickup code

---
+++ pickup.py
@@ -0,0 +1,32 @@
+import common
+
+__all__ = ['Roller']
+
+class Roller(common.ComponentBase):
+	
+	def __init__(self, config):
+		self.motor = config.pickup_motor
+		self.solenoid = config.pickup_solenoid
+
+		self.forward = config.forward
+		self.reverse = config.reverse
+
+		self.out_button = config.out_button
+		self.in_button = config.in_button
+		self.motor_button = config.motor_button
+
+		self.speed_axis = config.speed_axis
+
+	def op_init(self):
+		pass
+
+	def op_tick(self, time):
+		speed = 0
+		if self.motor_button.get():
+			speed = self.speed_axis.get()
+		self.motor.Set(speed)
+
+		if self.out_button.get():
+			self.solenoid.Set(self.forward)
+		if self.in_button.get():
+			self.solenoid.Set(self.reverse)
