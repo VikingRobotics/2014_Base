d92b018365a58989be0167ddf728531034a3c84c
HappyFox <keith.baston@gmail.com>
2014-02-12 20:47:20 -0800

    Some changes to the shooter code.
    Hopefully this make the code more reliable… I hope.
    Also add a timeout for the shooter.

--- shooter.py
+++ shooter.py
@@ -12,8 +12,10 @@ class Shooter(common.ComponentBase):
         self.stop_buttons = config.stop_buttons
         self.stop_inputs = config.stop_inputs
         self.reset_stop = config.reset_stop
+        self.max_throw_time = config.max_throw_time
 
         self.current_stop = 0
+        self.throw_start_time = 0.0
 
         self.pressed = False
 
@@ -21,43 +23,62 @@ class Shooter(common.ComponentBase):
         self.motors.Set(0)
 
     def op_tick(self, time):
-        prev = self.pressed
-        self.pressed = self.shoot_button.get()
-        if not prev and self.pressed:
-            reseting = False
+
+        throw_time = time - self.throw_start_time
 
         speed = 0
-        self.current_stop = self.get_current_stop()
-        if not self.should_stop(self.current_stop) and not self.reseting:
-            speed = 1
-        else:
-            self.reseting = True
 
-        if self.reseting and not self.reset_stop.Get():
-            speed = -.1
+        if not throw_time >= self.max_throw_time:
+
+            prev = self.pressed
+            self.pressed = self.shoot_button.get()
+
+            if not prev and self.pressed:
+                self.reseting = False
+                self.current_stop = self.get_current_stop()
+                self.throw_start_time = time
+
+            if not self.should_stop(self.current_stop) and not self.reseting:
+
+                speed = 1
+            else:
+                self.reseting = True
+
+            if self.reseting
+                self.throw_start_time = time
+                if self.reset_stop.Get():
+                    speed = 0
+                else:
+                    speed = -.1
 
         print(speed)
 
         """
-        When we are all in agreement that this code won't blow things up 
+        When we are all in agreement that this code won't blow things up
         uncomment the line below to enable actual movement of the Shooter
         arm.
-        I would suggest pretty thuroughly testing this by manually moving
-        the arm along the sensors and editing until the desired behavior 
-        is acheived.
+        I would suggest pretty thoroughly testing this by manually moving
+        the arm along the sensors and editing until the desired behavior
+        is achieved.
         """
         #self.motors.Set(speed)
-        
+
 
 
     def get_current_stop(self):
-        for i in range(len(self.stop_buttons)):
-            if self.stop_buttons[i].get():
-                return i
+        for idx, button in enumerate(self.stop_buttons):
+            if button.get():
+                return idx
+
+        # If we don't detect any buttons go for the most restrictive.
+        # That way if the switch malfunctions or we don't read it, we just
+        # stop.
+        return 0
 
     def should_stop(self, current_stop):
-        for stop_num in range(len(self.stop_inputs)):
-            if stop_num >= current_stop and stop_inputs[stop_num].Get():
+        for idx, stop_input in enumerate(self.stop_inputs):
+            if stop_num >= idx and stop_input.Get():
                 return True
 
         return False
+
