6f542001a3b287bd2b32c6b99236d53310851a03
Paul <blackdiamond377@gmail.com>
2014-04-03 15:38:18 -0700

    Changed to measurement based derivative and a few other things. Should be pretty much fully functional at this point.

--- src/pidController.py
+++ src/pidController.py
@@ -6,28 +6,43 @@ class PIDController(object):
         self.kd = d
 
         self.in_var = in_var
-        self.setpoint = setpoint
-
-        self.p_error = setpoint - in_var
-        self.error_sum = self.p_error
+        self.p_input = 0
         
+        set_setpoint(setpoint)
+
+        self.i_term = 0
+
         self.max_out = 1
         self.min_out = -1
 
-    def compute(self, delta_time, in_var):
-        self.error = self.setpoint - in_var()
-        self.error_sum += self.error * delta_time
+        self.enabled = True
 
-        d_error = (self.error - self.p_error) / delta_time
+    def compute(self):
+        if not self.enabled:
+            reutrn
 
-        output = self.kp*self.error + self.ki*self.error_sum + self.kd*d_error
+        _input = self.in_var()
+        
+        self.error = self.setpoint - _input
+
+        self.i_term += self.ki * self.error
+        if self.i_term > self.max_out:
+            self.i_term = self.max_out
+        elif self.i_term < self.min_out:
+            self.i_term = self.min_out
+
+        d_input = _input - self.p_input
+
+        self.output = self.kp*self.error + self.i_term + self.kd*d_input
 
         if output > self.max_out:
             output = self.max_out
         elif output < self.min_out:
             output = self.min_out
 
-        return output
+        self.p_input = _input
+
+        return self.output
 
     def set_vars(self, kp, ki, kd):
         self.kp = kp
@@ -45,3 +60,14 @@ class PIDController(object):
 
         self.max_out = _max
         self.min_out = _min
+
+    def enable(self):
+        self.enabled = True
+        reinitialize()
+
+    def disable(self):
+        self.enabled = False
+
+    def reinitialize(self):
+        self.p_input = self.in_var()
+        self.i_term = self.output
