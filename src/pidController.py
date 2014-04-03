1c029b5d39b9d0270718b312e989e2ea8228cb2a
blackdiamond377 <blackdiamond377@gmail.com>
2014-04-02 23:09:09 -0700

    Added the beginning of a pid controller. Its not pretty, but it should work....

---
+++ src/pidController.py
@@ -0,0 +1,27 @@
+
+class PIDController(object):
+	def __init__(self, p, i, d, in_var, setpoint):
+		self.kp = p
+		self.ki = i
+        self.kd = d
+
+        self.in_var = in_var
+        self.setpoint = setpoint
+
+        self.p_error = setpoint - in_var
+        self.error_sum = self.p_error
+
+    def compute(self, delta_time, in_var):
+        self.error = self.setpoint - in_var()
+        self.error_sum = self.error * delta_time
+
+        d_error = (self.error - self.p_error) / delta_time
+
+        output = self.kp*self.error + self.ki*self.error_sum + self.kd*d_error
+
+        return output
+
+    def set_vars(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
